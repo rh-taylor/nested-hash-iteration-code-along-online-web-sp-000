781af75ef17a6813be4ce6e858d150acb41792a8
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-28 15:22:24 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, value|
   end
 end
